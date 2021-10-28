0da15464de27179e4a3df89978424e6391335758
DaksinWorld <onosov20071@gmail.com>
2021-10-28 12:43:37 +0300

    new commit

--- extensions/ckeditor5/admin/src/index.js
+++ extensions/ckeditor5/admin/src/index.js
@@ -1,26 +1,31 @@
 import pluginPkg from '../../package.json';
-import Wysiwyg from './components/Wysiwyg';
 import pluginId from './pluginId';
+import Initializer from './containers/Initializer';
+import lifecycles from './lifecycles';
+import trads from './translations';
+
+import Wysiwyg from './components/Wysiwyg';
 
 export default strapi => {
   const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
+  const icon = pluginPkg.strapi.icon;
+  const name = pluginPkg.strapi.name;
 
   const plugin = {
     blockerComponent: null,
     blockerComponentProps: {},
     description: pluginDescription,
-    icon: pluginPkg.strapi.icon,
+    icon,
     id: pluginId,
-    initializer: () => null,
+    initializer: Initializer,
     injectedComponents: [],
-    isReady: true,
+    isReady: false,
     isRequired: pluginPkg.strapi.required || false,
-    mainComponent: null,
-    name: pluginPkg.strapi.name,
+    layout: null,
+    lifecycles,
+    name,
     preventComponentRendering: false,
-    settings: null,
-    trads: {},
-
+    trads
   };
 
   strapi.registerField({ type: 'wysiwyg', Component: Wysiwyg });
